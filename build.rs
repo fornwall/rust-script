187a7f4b9f312d48a7c0e2a1c6bb293bc3374a16
Daniel Keep <daniel.keep@gmail.com>
2017-04-10 19:33:57 +1000

    Updated copyright.

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
+/*
+Copyright ⓒ 2017 cargo-script contributors.
+
+Licensed under the MIT license (see LICENSE or <http://opensource.org
+/licenses/MIT>) or the Apache License, Version 2.0 (see LICENSE of
+<http://www.apache.org/licenses/LICENSE-2.0>), at your option. All
+files in the project carrying such notice may not be copied, modified,
+or distributed except according to those terms.
+*/
 extern crate rustc_version;
 use rustc_version::{version_matches};
 
