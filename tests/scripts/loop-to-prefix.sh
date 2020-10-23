faf79ac7aefa70832ffef3494b64c27553b3023c
Fredrik Fornwall <fredrik@fornwall.net>
2020-10-23 16:30:50 +0200

    Fix failing test

--- tests/scripts/loop-to-prefix.sh
+++ tests/scripts/loop-to-prefix.sh
@@ -6,6 +6,5 @@ echo "line1\nline2" | rust-script --loop \
     "let mut n=0; move |l| {n+=1; println!(\"{:>6}: {}\",n,l.trim_right())}"
 
 echo "Second:"
-echo asdf
 echo "line1\nline2" | rust-script --count --loop \
     "|l,n| println!(\"{:>6}: {}\", n, l.trim_right())"
