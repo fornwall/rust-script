96c5fe84a4d4d26c87224143f80ad41cc5736859
Fredrik Fornwall <fredrik@fornwall.net>
2020-10-25 02:33:36 +0200

    Avoid a warning in 2018 edition

--- src/consts.rs
+++ src/consts.rs
@@ -53,7 +53,7 @@ fn main() {
     }
 }
 
-fn try_main() -> Result<(), Box<std::error::Error>> {
+fn try_main() -> Result<(), Box<dyn std::error::Error>> {
     match {#{script}} {
         __cargo_script_expr => println!("{:?}", __cargo_script_expr)
     }
