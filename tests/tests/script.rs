a6c57b96a21741cc4e8a9df5c963fbb52049f212
Fredrik Fornwall <fredrik@fornwall.net>
2020-11-04 12:56:17 +0100

    Fix for when running test on Windows
    Windows says
      "Error: Os { code: 2, kind: NotFound, message: \"The system cannot find the file specified.\" }\n"
    while Linux and Mac has:
      "Error: Os { code: 2, kind: NotFound, message: \"No such file or directory\" }\n"

--- tests/tests/script.rs
+++ tests/tests/script.rs
@@ -148,8 +148,7 @@ fn script_with_same_name_as_dependency() {
 #[test]
 fn script_without_main_question_mark() {
     let out = rust_script!("tests/data/question-mark").unwrap();
-    assert_eq!(
-        out.stderr,
-        "Error: Os { code: 2, kind: NotFound, message: \"No such file or directory\" }\n"
-    );
+    assert!(out
+        .stderr
+        .starts_with("Error: Os { code: 2, kind: NotFound, message:"));
 }
