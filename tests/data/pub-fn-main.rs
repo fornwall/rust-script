5d6d9ebfd13b0f654268585477b2288923522c34
Fredrik Fornwall <fredrik@fornwall.net>
2021-01-24 22:13:05 +0100

    Fix test

--- tests/data/pub-fn-main.rs
+++ tests/data/pub-fn-main.rs
@@ -1,11 +1,9 @@
 //! ```cargo
 //! [dependencies]
 //! boolinator = "=0.1.0"
-//! tokio = { version = "1", features = ["full"] }
 //! ```
 use boolinator::Boolinator;
 
-#[tokio::main]
 pub fn main() {
     println!("--output--");
     println!("{:?}", true.as_some(1));
