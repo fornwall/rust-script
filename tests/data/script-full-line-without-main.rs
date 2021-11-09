f53aaa09bc675fc0273ade9e7688a5279d64dd2f
Fredrik Fornwall <fredrik@fornwall.net>
2021-11-09 23:27:02 +0100

    Fix warning in test

--- tests/data/script-full-line-without-main.rs
+++ tests/data/script-full-line-without-main.rs
@@ -1,12 +1,10 @@
 #!/usr/bin/env rust-script
-/*!
-This is merged into a default manifest in order to form the full package manifest:
-
-```cargo
-[dependencies]
-boolinator = "=0.1.0"
-```
-*/
+/// This is merged into a default manifest in order to form the full package manifest:
+///
+/// ```cargo
+/// [dependencies]
+/// boolinator = "=0.1.0"
+/// ```
 use boolinator::Boolinator;
 
 println!("--output--");
